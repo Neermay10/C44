f25b3dc5f4dda3c708dc100de1eae427ede72522
Neermay10 <74848380+Neermay10@users.noreply.github.com>
2021-03-15 16:04:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+var name, girlImage, boyImage;
+var healthPoints, progressBar;
+var Cash
+var workProfile, dishWasher, clerk, teacher, policeOfficer, bussinessMan;
+var foodProfile, junkfood, fruits, regularMeal, premiumMeal;
